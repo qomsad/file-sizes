33111b87ee67f78bf3c44bcf4f23a945ec0a6d72
qomsad <149834513+qomsad@users.noreply.github.com>
2024-05-13 01:31:19 +0500

    Create file_sizes.ps1

---
+++ file_sizes.ps1
@@ -0,0 +1,12 @@
+$dataColl = @()
+gci -force $pwd -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
+$len = 0
+gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+$foldername = $_.fullname
+$foldersize= '{0:N2}' -f ($len / 1Gb)
+$dataObject = New-Object PSObject
+Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldername” -value $foldername
+Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldersizeGb” -value $foldersize
+$dataColl += $dataObject
+}
+$dataColl | Out-GridView -Title "Size of subdirectories"
